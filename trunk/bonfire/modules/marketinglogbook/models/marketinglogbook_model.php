ef5ef1e2575b220ae761184f95044b322b73136f
aditya.satrya <aditya.satrya@8ab77323-3742-702c-0238-1a41ea9cd093>
2012-02-13 17:08:40 +0000

    Add SUM for all amount column in Marketinglogbook module

--- trunk/bonfire/modules/marketinglogbook/models/marketinglogbook_model.php
+++ trunk/bonfire/modules/marketinglogbook/models/marketinglogbook_model.php
@@ -14,13 +14,13 @@ class Marketinglogbook_model extends BF_Model {
 		return $this->select("
 			`justification`.`id`, `usergroup_name`, `marketingactivity_name`, `activitytype_name`, `product_name`,
 
-			`justification_approval_date`, `justification_description`, `justification_amount`, `justification_number`, `justification_creation_date`, 
+			`justification_id`, `justification_approval_date`, `justification_description`, `justification_amount`, `justification_number`, `justification_creation_date`, 
 
-			`purchaserequisition_number`, `purchaserequisition_vendor`, `purchaserequisition_job_number`, `purchaserequisition_creation_date`, `purchaserequisition_start_circulation_date`, `purchaserequisition_approve_manager_date`, `purchaserequisition_approve_vp_date`, `purchaserequisition_complete_circulation_date`, `purchaserequisition_submit_proc_date`, 
+			`purchaserequisition_id`, `purchaserequisition_number`, `purchaserequisition_vendor`, `purchaserequisition_job_number`, `purchaserequisition_creation_date`, `purchaserequisition_start_circulation_date`, `purchaserequisition_approve_manager_date`, `purchaserequisition_approve_vp_date`, `purchaserequisition_complete_circulation_date`, `purchaserequisition_submit_proc_date`, 
 
-			`purchaseorder_number`, `purchaseorder_received_date`, `purchaseorder_promised_date`, `purchaseorder_amount`, `purchaseorder_submit_date`, 
+			`purchaseorder_invoice.purchaseorder_id`, `purchaseorder_number`, `purchaseorder_received_date`, `purchaseorder_promised_date`, `purchaseorder_amount`, `purchaseorder_submit_date`, 
 
-			`invoice_bast_date`, `invoice_received_oracle_date`, `invoice_received_amount`, `invoice_remark_po`, `invoice_number`, `invoice_received_date`, `invoice_amount`, `invoice_sign_manager_date`, `invoice_complete_1_date`, `invoice_sign_vp_date`, `invoice_complete_2_date`, `invoice_submit_date`"
+			`invoice_id`, `invoice_bast_date`, `invoice_received_oracle_date`, `invoice_received_amount`, `invoice_remark_po`, `invoice_number`, `invoice_received_date`, `invoice_amount`, `invoice_sign_manager_date`, `invoice_complete_1_date`, `invoice_sign_vp_date`, `invoice_complete_2_date`, `invoice_submit_date`"
 		)->find_all();
 	}
 	
